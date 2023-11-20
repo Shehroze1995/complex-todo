a2c02fdcba1223415e93760e612a9bb0b399ee85
Shehroze Khan <shehrozeahmad@yahoo.com>
2023-11-20 13:35:12 +0500

    made responsive

--- src/routes/GetStarted.jsx
+++ src/routes/GetStarted.jsx
@@ -1,11 +1,10 @@
 import { useState } from "react";
-import getStartedSvg from "../assets/getStarted.png";
 import { MdRemoveRedEye } from "react-icons/md";
 
 const GetStarted = () => {
   const [getStarted, setGetStarted] = useState(false);
   return (
-    <div className="grid grid-cols-2 p-8 max-w-[1800px] m-auto max-[900px]:block">
+    <div className="grid grid-cols-2 p-8 min-h-screen max-w-[1800px] m-auto max-[900px]:block max-[900px]:p-4 max-[900px]:bg-[#111111]">
       <div className="bg-[#111111] relative h-[calc(100vh-64px)] flex place-items-center place-content-center rounded-lg max-[900px]:hidden">
         <h2 className="text-white absolute top-8 left-8 text-4xl font-extrabold">
           Todo <br />
@@ -21,15 +20,17 @@ const GetStarted = () => {
       <div className="flex place-items-center justify-center h-[calc(100vh-64px)]">
         {!getStarted ? (
           <section className="w-9/12 m-auto grid gap-4 max-[900px]:w-full">
-            <h2 className="text-4xl font-extrabold">Productive Mind</h2>
-            <p className="text-gray-700">
+            <h2 className="text-4xl font-extrabold max-[900px]:text-white">
+              Productive Mind
+            </h2>
+            <p className="text-gray-700 max-[900px]:text-gray-300">
               With only the features you need, Todo App is customized for
               individuals seeking a stress-free way to stay focused on their
               goals, projects, and tasks.
             </p>
             <button
               onClick={() => setGetStarted(true)}
-              className="bg-[#FFD43B] w-full py-2 rounded-md transition-all duration-300 hover:bg-[#111111] hover:text-white"
+              className="text-xl bg-[#FFD43B] w-full py-2 rounded-md transition-all duration-300 hover:bg-[#111111] hover:text-white"
               type="button"
             >
               Get Started
@@ -37,7 +38,9 @@ const GetStarted = () => {
           </section>
         ) : (
           <section className="w-9/12 m-auto grid gap-4 max-[900px]:w-full">
-            <h2 className="text-4xl font-extrabold">Sign in</h2>
+            <h2 className="text-4xl font-extrabold max-[900px]:text-white">
+              Sign in
+            </h2>
             <input
               className="border border-gray-300 w-full rounded-lg py-1 pl-3 outline-none cursor-not-allowed placeholder:text-black"
               readOnly
@@ -58,17 +61,17 @@ const GetStarted = () => {
               <MdRemoveRedEye className="absolute top-[50%] translate-y-[-50%] right-3 cursor-not-allowed" />
             </div>
             <button
-              className="bg-[#FFD43B] w-full py-2 rounded-md transition-all duration-300 hover:bg-[#111111] hover:text-white"
+              className="text-xl bg-[#FFD43B] w-full py-2 rounded-md transition-all duration-300 hover:bg-[#111111] hover:text-white"
               type="button"
             >
               Sign in
             </button>
             <div className="flex place-items-center">
-              <div className="border-2 border-gray-300 flex-1" />
-              <p className="mx-2">or</p>
-              <div className="border-2 border-gray-300 flex-1" />
+              <div className="border-2 border-gray-300 flex-1 max-[900px]:border max-[900px]:border-gray-800" />
+              <p className="mx-2 text-gray-600 max-[900px]:text-gray-300">OR</p>
+              <div className="border-2 border-gray-300 flex-1 max-[900px]:border max-[900px]:border-gray-800" />
             </div>
-            <div className="flex gap-2">
+            <div className="flex gap-2 text-xl">
               <button
                 className="bg-gray-300 flex-1 py-1 rounded-lg text-orange-500"
                 type="button"
@@ -82,7 +85,7 @@ const GetStarted = () => {
                 Facebook
               </button>
             </div>
-            <p className="text-center text-gray-600">
+            <p className="text-center text-gray-600 max-[900px]:text-gray-300">
               Don&apos;t have an account? Sign up
             </p>
           </section>
