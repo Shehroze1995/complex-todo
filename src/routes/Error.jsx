669781864b8883df50b5819e082653e6891d8298
Shehroze Khan <shehrozeahmad@yahoo.com>
2023-11-22 14:48:27 +0500

    updated error page

--- src/routes/Error.jsx
+++ src/routes/Error.jsx
@@ -1,6 +1,6 @@
 const Error = () => {
   return (
-    <div>
+    <div className="w-full h-screen flex flex-col text-2xl place-items-center place-content-center">
       <p>404 Error</p>
       <p>No page found!</p>
     </div>
